ed92d482c1faad508345fb2214c17a1ba8523a74
Shafik Bahou <snb3000@gmail.com>
2024-07-27 00:58:17 +0000

    finalized endpoints and project

--- src/app.py
+++ src/app.py
@@ -1,2 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
+todos = [
+    {
+        "label": "Sample Todo 1",
+        "done": True
+    },
+    {
+        "label": "Sample Todo 2",
+        "done": False
+    }
+]
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if 0 <= position < len(todos):
+        todos.pop(position)
+        return jsonify(todos)
+    else:
+        return jsonify({"error": "Invalid position"}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
